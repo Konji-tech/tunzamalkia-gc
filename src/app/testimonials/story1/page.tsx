3dcaebb73ec5c12e6d896be6ff6cedfcff616545
Konji-tech <Konjiviola@gmail.com>
2024-09-02 19:24:01 +0300

    cleaning UI

--- src/app/testimonials/story1/page.tsx
+++ src/app/testimonials/story1/page.tsx
@@ -27,7 +27,7 @@ const Story1 = () => {
   return (
     <div className="p-8 bg-white w-screen h-screen">
       <h1 className="text-3xl font-bold mb-4 text-center">Finding My Voice</h1>
-      <div className="font-poppins p-16 text-justify">
+      <div className="font-poppins p-16 text-justify font-bold">
         <p>
           I was 22 when I first realized how little I knew about my own body. Growing up, conversations about sex were shrouded in shame and secrecy. In my
           community, the idea of discussing anything remotely related to sexual health was taboo. So, like many other girls, I learned to stay quiet. I learned
@@ -37,7 +37,7 @@ const Story1 = () => {
           part of my life completely in the dark. It wasnt until a friend shared her story—her own struggles, confusion, and eventual empowerment—that I
           realized I wasnt alone. Her openness was like a door had been unlocked for me. I started asking questions, and slowly, I began finding the answers I
           had been seeking for so long. I learned that my health, my body, and my well-being were nothing to be ashamed of. I learned that talking about these
-          things didnt make me weak—it made me strong.Now, I share my story in the hope that it might help another woman, just like my friend’s story helped
+          things didnt make me weak—it made me strong.Now, I share my story in the hope that it might help another woman, just like my friend,s story helped
           me. We dont have to face these challenges alone. Together, we can break the silence and support each other on our journeys to understanding, healing,
           and empowerment.
         </p>
@@ -46,8 +46,8 @@ const Story1 = () => {
       <p className="mb-4 text-justify">Your voice can inspire change.
         Share your story anonymously and help other women feel seen, heard, and supported.When comfortable and ready,click below to share your story </p>
       <Link href="https://docs.google.com/forms/d/e/1FAIpQLSfIflscaYftjiCQ3c0bxSlmB-uWofJTojyEBk0ML-OdaEQbTQ/viewform">
-        <button className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5">
-          Share Your Testimonial
+        <button className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300  rounded-lg text-sm px-5 py-2.5 font-bold">
+          Share Your Story!
         </button>
       </Link>
     </div>
