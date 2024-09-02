e966a50df036e342ebdb0bc62b202abc81c80998
Konji-tech <konjiviola@gmail.com>
2024-09-02 21:38:16 +0300

    stories refactor

--- src/app/testimonials/page.tsx
+++ src/app/testimonials/page.tsx
@@ -1,73 +1,40 @@
-'use client'
-
+"use client";
 
 import Image from "next/image";
-import chat from "@/../public/images/Bonding.svg";
-import questions from "@/../public/images/questions.svg";
 import Link from "next/link";
-import mainlanding from "@/../public/images/mainlanding.png";
-import ccs from "@/../public/images/age.svg";
 
-const featuresList = [
-  {
-    image: chat,
-    description:
-      "Find My Voice",
-    button: {
-      text: "Read More",
-      link: "/testimonials/story1",
-    },
-  },
-  {
-    image: ccs,
-    description:
-      "Coming of age.",
-    button: {
-      text: "Read More",
-      link: "/testimonials/story2",
-    },
-  },
-  {
-    image: mainlanding,
-    description: "My Community",
-    button: {
-      text: "Read More",
-      link: "/testimonials/story3",
-    },
-  },
-];
+import stories from "./stories";
 
 export default function FeatureList() {
   return (
-    <div>
+    <div className="bg-red-600 h-full flex flex-col bg-gradient-to-r from-indigo-600 to-fuchsia-400">
       {/* Stories Heading */}
-      <h1 className="text-3xl font-bold text-center my-8">Stories</h1>
-      <h2 className="text-2xl font-bold text-center my-10">Anonymous stories of Kenyan women</h2>
-
-      {/* Divider between sections */}
-      <div className="w-full h-8 bg-gradient-to-r from-indigo-600 to-fuchsia-400"></div>
+      <div className="flex flex-col gap-4 items-center py-24">
+        <h1 className="text-3xl font-bold text-center">Stories</h1>
+        <h2 className="text-xl font-medium text-center">
+          Anonymous stories of Kenyan women
+        </h2>
+      </div>
 
       {/* Feature List Section */}
-      <div className="bg-white py-32 grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center animate-fadeInRight">
-        {featuresList.map((feature, index) => (
+      <div className="bg-white py-32 flex items-center justify-center flex-col md:flex-row gap-8  h-full">
+        {stories.map((story, index) => (
           <div
             key={index}
-            className="flex flex-col items-center gap-4 w-full max-w-[350px] bg-white rounded-3xl shadow-lg p-6 transition duration-500 hover:scale-105"
+            className="flex flex-col items-center gap-4 w-full max-w-[350px] bg-white rounded-3xl shadow-lg p-6 transition duration-500 hover:scale-105 animate-fadeInRight"
           >
             <div className="rounded-3xl w-full aspect-square flex items-center justify-center bg-purple-600">
               <Image
-                src={feature.image}
+                src={story.image}
                 alt="feature image"
                 className="object-cover rounded-3xl"
               />
             </div>
-            <h3 className="text-center text-xl w-full px-4">
-              {feature.description}
-            </h3>
+            <h3 className="text-center text-xl w-full px-4">{story.title}</h3>
             <div className="w-full flex justify-center mt-4">
-              <Link href={feature.button.link}>
+              <Link href={"/testimonials/" + story.slug}>
                 <button className="text-white bg-gradient-to-r from-pink-400 via-pink-500 to-pink-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-pink-300 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                  {feature.button.text}
+                  Read more
                 </button>
               </Link>
             </div>
---
+++ src/app/testimonials/stories.ts
@@ -0,0 +1,27 @@
+import ccs from "@/../public/images/age.svg";
+import mainlanding from "@/../public/images/mainlanding.png";
+import chat from "@/../public/images/Bonding.svg";
+
+export default [
+  {
+    slug: "find-my-voice",
+    title: "Find my voice",
+    image: chat,
+    content:
+      " I was 22 when I first realized how little I knew about my own body. Growing up, conversations about sex were shrouded in shame and secrecy. In my community, the idea of discussing anything remotely related to sexual health was taboo. So, like many other girls, I learned to stay quiet. I learned to carry my questions in silence, convinced that asking would only make people think less of me. But the questions kept coming. I wondered about contraception, about whether the pain I felt was normal, about how to protect myself in relationships that didn't always feel safe. I googled things in secret, trying to piece together answers from articles that never seemed to understand my experience. I felt alone, like I was navigating this huge part of my life completely in the dark. It wasnt until a friend shared her story—her own struggles, confusion, and eventual empowerment—that I realized I wasnt alone. Her openness was like a door had been unlocked for me. I started asking questions, and slowly, I began finding the answers I had been seeking for so long. I learned that my health, my body, and my well-being were nothing to be ashamed of. I learned that talking about these things didnt make me weak—it made me strong.Now, I share my story in the hope that it might help another woman, just like my friend,s story helped me. We dont have to face these challenges alone. Together, we can break the silence and support each other on our journeys to understanding, healing, and empowerment. ",
+  },
+  {
+    slug: "coming-of-age",
+    title: "Coming of Age",
+    image: mainlanding,
+    content:
+      " I was 22 when I first realized how little I knew about my own body. Growing up, conversations about sex were shrouded in shame and secrecy. In my community, the idea of discussing anything remotely related to sexual health was taboo. So, like many other girls, I learned to stay quiet. I learned to carry my questions in silence, convinced that asking would only make people think less of me. But the questions kept coming. I wondered about contraception, about whether the pain I felt was normal, about how to protect myself in relationships that didn't always feel safe. I googled things in secret, trying to piece together answers from articles that never seemed to understand my experience. I felt alone, like I was navigating this huge part of my life completely in the dark. It wasnt until a friend shared her story—her own struggles, confusion, and eventual empowerment—that I realized I wasnt alone. Her openness was like a door had been unlocked for me. I started asking questions, and slowly, I began finding the answers I had been seeking for so long. I learned that my health, my body, and my well-being were nothing to be ashamed of. I learned that talking about these things didnt make me weak—it made me strong.Now, I share my story in the hope that it might help another woman, just like my friend,s story helped me. We dont have to face these challenges alone. Together, we can break the silence and support each other on our journeys to understanding, healing, and empowerment. ",
+  },
+  {
+    slug: "my-community",
+    title: "My Community",
+    image: ccs,
+    content:
+      " I was 22 when I first realized how little I knew about my own body. Growing up, conversations about sex were shrouded in shame and secrecy. In my community, the idea of discussing anything remotely related to sexual health was taboo. So, like many other girls, I learned to stay quiet. I learned to carry my questions in silence, convinced that asking would only make people think less of me. But the questions kept coming. I wondered about contraception, about whether the pain I felt was normal, about how to protect myself in relationships that didn't always feel safe. I googled things in secret, trying to piece together answers from articles that never seemed to understand my experience. I felt alone, like I was navigating this huge part of my life completely in the dark. It wasnt until a friend shared her story—her own struggles, confusion, and eventual empowerment—that I realized I wasnt alone. Her openness was like a door had been unlocked for me. I started asking questions, and slowly, I began finding the answers I had been seeking for so long. I learned that my health, my body, and my well-being were nothing to be ashamed of. I learned that talking about these things didnt make me weak—it made me strong.Now, I share my story in the hope that it might help another woman, just like my friend,s story helped me. We dont have to face these challenges alone. Together, we can break the silence and support each other on our journeys to understanding, healing, and empowerment. ",
+  },
+];
